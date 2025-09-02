88da9a241f91e8a2510301de993bec12a18e58c0
Henry <henry.devmotion@gmail.com>
2025-09-01 23:36:57 -0300

    commit update index/metas

--- multi-fiis/index.js
+++ multi-fiis/index.js
@@ -6,22 +6,26 @@ const [,, cmd, ...args] = process.argv;
 
 switch (cmd) {
     case "add-fii":
-        addFii (args[0], Number([1]) || 0);
+        addFii(args[0], Number([i]) || 0);
         break;
+
     case "list-fii":
-        listFiis ();
+        listFiis();
         break;
+
     case "add-meta":
-        addMeta (args[0], args[1] || "");
+        addMeta(args[0], args[i] || "");
         break;
+
     case "list-metas":
         listMetas();
         break;
-        default:
-            console.log(`Comandos disponíveis:
-                add-fii <nome> <quantidade>       -> Adiciona um novo fundo
-                list-fii                          -> Lista os seus FII's
-                add-meta <descrição> <objetivos>  -> Adiciona uma meta 
-                list-metas                        -> Lista as metas
-                `);      
+
+    default:
+        console.log(`Comandos disponíveis:
+            add-fii <nome> <quantidade>       -> Adiciona um novo fundo
+            list-fii                          -> Lista os seus FII's
+            add-meta <descrição> <objetivos>  -> Adiciona uma meta 
+            list-metas                        -> Lista as metas
+            `);      
 }
--- multi-fiis/src/fii-metas.js
+++ multi-fiis/src/fii-metas.js
@@ -1,62 +1,67 @@
 import fs from "fs";
 import path from "path";
 
-const dataPath = path.resolve("data/fiis.json");
+const dataDir = path.resolve("./data");
+const fiiPath = path.join(dataDir, "fiis.json");
+const metaPath = path.join(dataDir, "meta.json");
 
-function loadData() {
-    if(!fs.existsSync(dataPath)) {
-        return { fiis: [], meta: [] };
-    }
-
-    const raw = fs.readFileSync(dataPath, "utf-8");
-    return JSON.parse(raw);
+if(!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir);
 }
 
-function saveData(data) {
-    fs.writeFileSync(dataPath, JSON.stringify(data, null, 2), "utf-8");
+function carregarArquivos(filePath) {
+    if (!fs.existsSync(filePath)) return [];
+    return JSON.parse(fs.readFileSync(filePath, "utf-8"));
+}
+
+function salvarArquivos(filePath, data) {
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf-8");
+
 }
 
 export function addFii(nome, quantidade) {
-    const data = loadData();
-
-    if (data.fiis.some(f => f.nome.toLowerCase() === nome.toLowerCase())) {
-        console.log(`O Fundo ${nome} já existe na sua carteira.`);
-        return;
-    }
-    data.fiis.push({nome, quantidade});
-    saveData(data);
-    console.log(`Adicionado ${quantidade} em cotas do fundo ${nome}`);
+    const fiis = carregarArquivos(fiiPath);
+    fiis.push({ nome, quantidade });
+    salvarArquivos(fiiPath, fiis);
+    console.log(`Foram adicionadas ${quantidade} cotas do fundo $`);
 }
 
 export function listFiis() {
-     const data = loadData();
-     if(data.fiis.length === 0) {
+    const fiis = carregarArquivos(fiiPath);
+    if (fiis.length === 0) {
         console.log("Nenhum fundo adicionado.");
         return;
-     }
-
-     console.log("Seus FIIS:");
-     data.fiis.forEach(f => {
-        console.log(`- ${f.nome} | ${f.quantidade} cotas`);
-     });
+    }
+    
+    console.log("Sua lista de Fundos:");
+    fiis.forEach((f, i) => {
+        console.log(`- ${i + 1}`)
+    })
 }
 
 export function addMeta(descricao, objetivo) {
-    const data = loadData();
-    data.metas.push({descricao,objetivo, atingido: false});
-    saveData(data);
-    console.log(`Meta adicionada: ${descricao} (objetivo: ${objetivo})`);
+    const metas = carregarArquivos(metaPath);
+    metas.push({
+        descricao, 
+        objetivo, 
+        atingido: false
+    });
+    salvarArquivos(metaPath, metas);
+    console.log(`Meta adicionada: "${descricao}" objetivo: "${objetivo}"`);
 }
 
 export function listMetas() {
-    const data= loadData();
-    if (data.metas.length === 0) {
+    const metas = carregarArquivos(metasPath);
+    if (metas.length === 0) {
         console.log("Nenhuma meta definida");
         return;
     }
 
-    console.log("Suas metas:");
-    data.metas.forEach((m, i) => {
-        console.log(`- ${i + 1}, ${m.descricao} | objetivo: ${m.objetivo} | Status: ${m.atingido ? "Concluído" : "Em andamento"}`);
+
+console.log("Suas Metas:");
+    metas.forEach((m, i) => {
+        console.log(
+            `- ${i + 1}. ${m.descricao} | objetivo: ${m.objetivo} | Status: ${m.atingido ? "Concluído" : "Em andamento"}`
+        );
     });
 }
